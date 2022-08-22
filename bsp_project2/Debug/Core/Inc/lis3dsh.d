b5a1ccca82361715f25a883a2572efaf3f49ccf1
plh81hc <thanhluanpham@live.com>
2022-08-22 09:26:01 +0700

    first commit

---
+++ bsp_project2/Debug/Core/Inc/lis3dsh.d
@@ -0,0 +1,4 @@
+Core/Inc/lis3dsh.o: ../Core/Inc/lis3dsh.c ../Core/Inc/lis3dsh.h \
+ c:\users\luan_pham\downloads\stm32cube_fw_f4_v1.27.0\drivers\bsp\components\common\accelero.h
+../Core/Inc/lis3dsh.h:
+c:\users\luan_pham\downloads\stm32cube_fw_f4_v1.27.0\drivers\bsp\components\common\accelero.h:
