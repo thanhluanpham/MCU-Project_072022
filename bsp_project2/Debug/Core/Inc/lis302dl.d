561aa5b782d07e5a359723e0edae1a1307bd9b65
plh81hc <thanhluanpham@live.com>
2022-10-11 22:54:57 +0700

    up

--- bsp_project2/Debug/Core/Inc/lis302dl.d
+++
@@ -1,4 +0,0 @@
-Core/Inc/lis302dl.o: ../Core/Inc/lis302dl.c ../Core/Inc/lis302dl.h \
- e:\workspace\spi_workspace\bsp_project2\drivers\common\accelero.h
-../Core/Inc/lis302dl.h:
-e:\workspace\spi_workspace\bsp_project2\drivers\common\accelero.h:
--- bsp_project2/Debug/Core/Inc/lis3dsh.d
+++
@@ -1,4 +0,0 @@
-Core/Inc/lis3dsh.o: ../Core/Inc/lis3dsh.c ../Core/Inc/lis3dsh.h \
- e:\workspace\spi_workspace\bsp_project2\drivers\common\accelero.h
-../Core/Inc/lis3dsh.h:
-e:\workspace\spi_workspace\bsp_project2\drivers\common\accelero.h:
